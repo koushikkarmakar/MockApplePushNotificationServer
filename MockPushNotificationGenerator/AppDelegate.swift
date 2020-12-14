8bd17fa945bbcfda84efd7340856b7f63ea296a1
Koushik Karmakar <koushik.karmakar@tum.de>
2020-12-14 11:30:05 +0100

    Initial Commit

---
+++ MockPushNotificationGenerator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MockPushNotificationGenerator
+//
+//  Created by Koushik Karmakar on 14.12.20.
+//  Copyright © 2020 Koushik Karmakar. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MockPushNotificationGenerator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MockPushNotificationGenerator
+//
+//  Created by Koushik Karmakar on 14.12.20.
+//  Copyright © 2020 Koushik Karmakar. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
