424271d0e780735e34c6725ef01a1120cf97bec1
Koushik Karmakar <koushik.karmakar@tum.de>
2020-12-14 15:38:59 +0100

    changes

--- MockPushNotificationGenerator/AppDelegate.swift
+++ MockPushNotificationGenerator/AppDelegate.swift
@@ -11,10 +11,11 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    let serverManager = ServerManager()
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        serverManager.startServer()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
