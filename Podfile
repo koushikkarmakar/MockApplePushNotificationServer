3b93a500480831fb15a3eff2535cffafdcf6f822
Koushik Karmakar <koushik.karmakar@tum.de>
2021-01-04 00:04:51 +0100

    removed unnecessary pods

--- Podfile
+++ Podfile
@@ -1,13 +1,8 @@
-# Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
-
 target 'MockPushNotificationGenerator' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
   # Pods for MockPushNotificationGenerator
-  pod 'SwiftyJSON', '~> 4.0'
-  pod 'SwiftLint'
   pod 'Telegraph'
 
   target 'MockPushNotificationGeneratorTests' do
--- Pods/Target Support Files/Pods-MockPushNotificationGenerator/Pods-MockPushNotificationGenerator-frameworks.sh
+++ Pods/Target Support Files/Pods-MockPushNotificationGenerator/Pods-MockPushNotificationGenerator-frameworks.sh
@@ -163,13 +163,11 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/CocoaAsyncSocket/CocoaAsyncSocket.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/HTTPParserC/HTTPParserC.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Telegraph/Telegraph.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/CocoaAsyncSocket/CocoaAsyncSocket.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/HTTPParserC/HTTPParserC.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Telegraph/Telegraph.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
